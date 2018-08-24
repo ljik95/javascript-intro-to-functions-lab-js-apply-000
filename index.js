0a679cb226ba9c7d232d072ebdc3abe339513f01
Jongik Thom <ljik95@hotmail.com>
2018-08-24 14:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) {
+    return 'YES INDEED!';
+  }
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
+  }
+}
